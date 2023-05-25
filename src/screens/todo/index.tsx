87d54c6a702c3623728abdaa9b6b3771570a0165
dxdns <dxdns@hotmail.com>
2023-05-25 11:49:22 -0300

    greeting title header

--- src/screens/todo/index.tsx
+++ src/screens/todo/index.tsx
@@ -2,17 +2,54 @@ import {
 	Box,
 	Center,
 	Flex,
+	HStack,
 	Heading,
+	Icon,
 } from "native-base";
 import TodoDetails from "./details";
+import { useEffect, useState } from "react";
+import IconFeather from "react-native-vector-icons/Feather";
 
 export default function Todo() {
+	const [greeting, setGreeting] = useState("");
+	const [icon, setIcon] = useState("");
+
+	const getGreeting = () => {
+		var d = new Date();
+		var time = d.getHours();
+
+		if (time < 12) {
+			return ["Good morning", "sun"];
+		} else if (time < 18) {
+			return ["good afternoon", "sun"];
+		}
+
+		return ["Good evening", "moon"];
+	}
+
+	useEffect(() => {
+		const [greetingText, greetingIcon] = getGreeting();
+		setGreeting(greetingText);
+		setIcon(greetingIcon);
+	}, [])
+
 	return (
 		<Flex w={"100%"} h={"100%"} alignItems="center">
 			<Center w={"100%"} h={"20%"}>
-				<Heading size="lg">
-					TodoList
-				</Heading>
+				<HStack space={3}>
+					<Icon
+						as={<IconFeather name={icon} />}
+						size={35}
+						name="greetingIcon"
+						color="coolGray.800"
+						_dark={{
+							color: "warmGray.50"
+						}}
+					/>
+					<Heading size="lg">
+						{greeting}
+					</Heading>
+				</HStack>
 			</Center>
 			<Center
 				bg="primary.700"
